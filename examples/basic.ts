4cb5309fcf938cea234c4845562967f978f6bcea
Junho Yeo <i@junho.io>
2025-04-29 01:50:08 +0900

    Use PDF files in current directory

--- examples/basic.ts
+++ examples/basic.ts
@@ -1,6 +1,7 @@
 import { extractTextFromFileAsync } from '../index';
 import path from 'path';
 import clipboard from 'clipboardy';
+import { readdir } from 'fs/promises';
 
 const normalizeText = (text: string): string => {
   return text
@@ -12,11 +13,19 @@ const normalizeText = (text: string): string => {
 };
 
 const main = async () => {
-  const filePath = path.join(__dirname, 'example.pdf');
-  const text = await extractTextFromFileAsync(filePath)
-    .then(text => normalizeText(text));
-  console.log(text);
+  const files = await readdir(__dirname);
+  const pdfFiles = files.filter(file => file.endsWith('.pdf'));
+  console.log(pdfFiles);
 
+  let text = '';
+  for (const file of pdfFiles) {
+    const filePath = path.join(__dirname, file);
+    const contents = await extractTextFromFileAsync(filePath)
+      .then(text => normalizeText(text));
+      text += contents;
+    }
+    
+  console.log([text]);
   await clipboard.write(text);
 };
 
